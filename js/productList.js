ed7b1edc4fe9151b2bc04ca6cb73cb96a28235ed
champix56 <desorbaix@free.fr>
2023-04-07 13:15:26 +0200

    reformat

--- js/productList.js
+++ js/productList.js
@@ -1,47 +1,69 @@
 import { REST_SERVER_ADR } from "./commonVariables.js";
-
+let products = [];
 let productListTemplate;
 /**
- * 
- * @param {HTMLElement} wrapper node to wrap template in  
+ *
+ * @param {HTMLElement} wrapper node to wrap template in
  */
-export function loadAsyncProductList(wrapper){
-    if(undefined===productListTemplate)
-    {
-        fetch('/templates/produitsListe.html').then(r=>r.text()).then(h=>{
-            productListTemplate=h;
-            loadTemplate(wrapper);
-        })
-    }
-    else loadTemplate(wrapper);
+export function loadAsyncProductList(wrapper) {
+  if (undefined === productListTemplate) {
+    fetch("/templates/produitsListe.html")
+      .then((r) => r.text())
+      .then((h) => {
+        productListTemplate = h;
+        loadTemplate(wrapper);
+      });
+  } else loadTemplate(wrapper);
 }
 /**
  * sync function to wrap content of list template
- * @param {HTMLElement} wrapper node to wrap in  
+ * @param {HTMLElement} wrapper node to wrap in
  */
 function loadTemplate(wrapper) {
-    history.pushState(null, null, "/home");
-    wrapper.innerHTML     = productListTemplate;   
-    loadProductContent();
+  history.pushState(null, null, "/home");
+  wrapper.innerHTML = productListTemplate;
+  loadProductContent();
 }
-function loadProductContent(){
-/*recup des noeuds dans la structure , le container pour un produit a dupliquer et remplir et le parent pour mettre tous les enfants*/
-    let uniqueProductTemplate = document.querySelector(".produit-in-liste")
-    let wrapper=uniqueProductTemplate.parentNode;
-    uniqueProductTemplate.style.display='none';
-    //uniqueProductTemplate=uniqueProductTemplate.cloneNode(true);
-    fetch(REST_SERVER_ADR+'/produits').then(r=>r.json()).then(arr=>{
-        arr.map((element,index,liste)=>{
-            let produitElement= uniqueProductTemplate.cloneNode(true);
-            produitElement.id="produit-"+element.id;
-            produitElement.style.display="block";
-            produitElement.querySelector('h3').innerHTML=element.nom;
-            produitElement.querySelector('img').src=element.image;
-            produitElement.querySelector('.prix').innerHTML=element.prix.toFixed(2);
-            loadUniqueProduitEvent(produitElement);
-            wrapper.appendChild(produitElement);
-        });
+function loadProductContent() {
+  /*recup des noeuds dans la structure , le container pour un produit a dupliquer et remplir et le parent pour mettre tous les enfants*/
+  let uniqueProductTemplate = document.querySelector(".produit-in-liste");
+  let wrapper = uniqueProductTemplate.parentNode;
+  uniqueProductTemplate.style.display = "none";
+  //uniqueProductTemplate=uniqueProductTemplate.cloneNode(true);
+  fetch(REST_SERVER_ADR + "/produits")
+    .then((r) => r.json())
+    .then((arr) => {
+      products = arr;
+      arr.map((element, index, liste) => {
+        const productNode = makeAProductNode(uniqueProductTemplate, element);
+        wrapper.appendChild(productNode);
+      });
+    });
+}
+/**
+ * assemble a node from base templte with product data
+ * @param {HTMLElement} baseNodeToClone base template for a unique product
+ * @param {Object} product product object with data 
+ * @returns a HTML assembled node with product data
+ */
+function makeAProductNode(baseNodeToClone, product) {
+  let produitElement = baseNodeToClone.cloneNode(true);
+  produitElement.id = "produit-" + product.id;
+  produitElement.style.display = "block";
+  produitElement.querySelector("h3").innerHTML = product.nom;
+  produitElement.querySelector("img").src = product.image;
+  produitElement.querySelector(".prix").innerHTML = product.prix.toFixed(2);
+  loadUniqueProduitEvent(produitElement);
+  return produitElement;
+}
+/**
+ * add event on a product node from list
+ * @param {HTMLElement} produitElement 
+ */
+function loadUniqueProduitEvent(produitElement) {
+    produitElement.addEventListener('click',e=>{
+        if(confirm('Voulez vous ajouter ce produit au panier')){
+            alert('produit ajouté');
+        }
     })
-
 }
-function loadUniqueProduitEvent(produitElement){}

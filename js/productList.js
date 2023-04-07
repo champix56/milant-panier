3c0728a0954e930b3f8cef07dcc97bfb9a21ef49
champix56 <desorbaix@free.fr>
2023-04-07 14:31:56 +0200

    chargement des produits

--- js/productList.js
+++ js/productList.js
@@ -1,3 +1,5 @@
+import { REST_SERVER_ADR } from "./commonVariables.js";
+
 let productListTemplate;
 /**
  * 
@@ -22,5 +24,24 @@ function loadTemplate(wrapper) {
     wrapper.innerHTML     = productListTemplate;   
     loadProductContent();
 }
-function loadProductContent(){}
-function loadEvents(){}
+function loadProductContent(){
+/*recup des noeuds dans la structure , le container pour un produit a dupliquer et remplir et le parent pour mettre tous les enfants*/
+    let uniqueProductTemplate = document.querySelector(".produit-in-liste")
+    let wrapper=uniqueProductTemplate.parentNode;
+    uniqueProductTemplate.style.display='none';
+    //uniqueProductTemplate=uniqueProductTemplate.cloneNode(true);
+    fetch(REST_SERVER_ADR+'/produits').then(r=>r.json()).then(arr=>{
+        arr.map((element,index,liste)=>{
+            let produitElement= uniqueProductTemplate.cloneNode(true);
+            produitElement.id="produit-"+element.id;
+            produitElement.style.display="block";
+            produitElement.querySelector('h3').innerHTML=element.nom;
+            produitElement.querySelector('img').src=element.image;
+            produitElement.querySelector('.prix').innerHTML=element.prix.toFixed(2);
+            loadUniqueProduitEvent(produitElement);
+            wrapper.appendChild(produitElement);
+        });
+    })
+
+}
+function loadUniqueProduitEvent(produitElement){}
